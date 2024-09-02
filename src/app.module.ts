579cc27facf82b83d95239fa77c0f6d5a9b7a06e
Sasha Yatsykovych <olexandr.y@mate.academy>
2024-09-02 11:18:21 +0300

    add categories and events modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
+import { CategoriesModule } from './categories/categories.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [EventsModule, CategoriesModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
