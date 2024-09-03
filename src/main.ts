03e39c6d4b195c543a5c27a8f06cc45936698e65
Sasha Yatsykovych <olexandr.y@mate.academy>
2024-09-03 20:21:10 +0300

    add cors policy

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // Front-end client
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
   await app.listen(3002);
 }
 bootstrap();
